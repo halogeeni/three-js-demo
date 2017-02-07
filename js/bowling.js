1828991dfa66b148baabf735bec13640e8cbba23
Aleksi Rasio <aleksijr@metropolia.fi>
2017-02-07 13:51:17 +0200

    Add comments

--- js/bowling.js
+++ js/bowling.js
@@ -7,64 +7,76 @@
 	function init() {
 		// load JSON models
 		$.when(
-		$.getJSON('models/bowlingBall.json', function(data){
-			ballJSON = data;
+			$.getJSON('models/bowlingBall.json', function(data){
+				ballJSON = data;
 			}),
 			$.getJSON('models/bowlingPin.json', function(data){
-			pinJSON = data;
+				pinJSON = data;
 			})
-		).then( setupScene );
+		).then( setupScene );		// start scene setup after successful model loading
 		
 		function setupScene() {
+			// *** basic scene setup ***
 			// renderer
-			renderer = new THREE.WebGLRenderer( { antialias : true } );
+			renderer = new THREE.WebGLRenderer( { antialias : true } );		// create renderer, enable antialiasing
 			renderer.setSize( window.innerWidth, window.innerHeight );
-			renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-			document.body.appendChild( renderer.domElement );
+			renderer.shadowMap.type = THREE.PCFSoftShadowMap;				// enable softer shadows
+			document.body.appendChild( renderer.domElement );				// append canvas element to body
 
 			// scene
-			scene = new THREE.Scene();
-			scene.fog = new THREE.Fog( 0xcce0ff, 0, 500 );
+			scene = new THREE.Scene();							// create new Three.js scene
+			scene.fog = new THREE.Fog( 0xcce0ff, 0, 500 );		// a bit o' fog
 			
 			// camera
-			camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-			camera.position.z = 5;
+			camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );	// a pretty basic camera
+			camera.position.z = 5;		// set camera z axis position
 	
-			// lights
+	
+			// *** lights ***
 			
 			// ambient
-			var ambientLight = new THREE.AmbientLight( 0x000000 );
-			scene.add( ambientLight );
+			var ambientLight = new THREE.AmbientLight( 0x000000 );		// create new ambient light (="sun")
+			scene.add( ambientLight );									// add light to scene
 			
 			// spots
+			
+			// create an array of pointlights (= "bare lightbulbs")
 			var lights = [];
-			lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+			lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );			// parameters: (color, intensity, distance)
 			lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
 			lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
 
-			lights[ 0 ].position.set( 0, 30, 30 );
+			// set light xyz positions
+			lights[ 0 ].position.set( 0, 30, 30 );							
 			lights[ 1 ].position.set( 40, 50, -20 );
 			lights[ 2 ].position.set( -30, 60, -10 );
 			
+			// enable shadows for all lights 
 			for(var i = 0; i < lights.length; i++) {
 				lights[i].castShadow = true;
 				lights[i].shadow.camera.near = true;
 				scene.add(lights[i]);
 			}
 
-			// objects
 			
+			// *** objects ***
+			
+			// instantiate object loader for JSON models
 			var loader = new THREE.ObjectLoader();
 			
 			// plane
+			
+			// load plane texture from file
 			var planeTexture = new THREE.TextureLoader().load(
-				'textures/bw_checkered.jpg'
+				'./textures/bw_checkered.jpg'
 			);
+			// setup repeated texture wrapping
 			planeTexture.wrapS = THREE.RepeatWrapping;
 			planeTexture.wrapT = THREE.RepeatWrapping;
 			planeTexture.repeat = new THREE.Vector2(50, 50);
-			planeTexture.anisotropy = renderer.getMaxAnisotropy();
+			planeTexture.anisotropy = renderer.getMaxAnisotropy(); // setup texture anisotropy
 			
+			// define plane material
 			var planeMaterial = new THREE.MeshPhongMaterial({
 				color: 0x222222,
 				specular: 0xffffff,
@@ -72,17 +84,20 @@
 				shading: THREE.FlatShading,
 				map: planeTexture
 			});
-			var planeGeometry = new THREE.PlaneGeometry(500, 500);
-			var plane = new THREE.Mesh(planeGeometry, planeMaterial);
 			
-			plane.receiveShadow = true;
-			plane.rotation.x = -Math.PI / 2;
-			plane.position.y = -1;
+			var planeGeometry = new THREE.PlaneGeometry(500, 500);			// create plane geometry
+			var plane = new THREE.Mesh(planeGeometry, planeMaterial);		// create the plane mesh using geometry & material
 			
-			scene.add(plane);
+			plane.receiveShadow = true;				// this does not seem to work yet? no shadows are cast to the plane
+			plane.rotation.x = -Math.PI / 2;		// rotate plane
+			plane.position.y = -1;					// set plane y position
+			
+			scene.add(plane);						// add plane to scene
 
-			// bowling ball			
-			bowlingBall = loader.parse(ballJSON);
+			
+			// bowling ball
+			
+			bowlingBall = loader.parse(ballJSON);		// use loader to parse JSON model to Object3D-object
 			
 			// define ball material
 			var ballMaterial = new THREE.MeshStandardMaterial(
@@ -92,39 +107,41 @@
 					roughness : 0.5,
 					metalness : 0.38,
 					shading : THREE.SmoothShading,
-					vertexColors : THREE.NoColors,
-					fog : true
+					vertexColors : THREE.NoColors
 				}
 			);
 			
 			// Object3D is composed of meshes
-			// --> material must be added to all its children
+			// --> material must be added to all its children vertices
 			bowlingBall.traverse(function ( child ) {
 				if ( child instanceof THREE.Mesh ) {
 					child.material = ballMaterial;
 				}
 			});
 			
-			bowlingBall.position.set(2, 0, 0);
+			bowlingBall.position.set(2, 0, 0);		// set ball xyz position
+			// enable shadows on ball
 			bowlingBall.castShadow = true;
 			bowlingBall.receiveShadow = true;
-			
+			// add ball to scene
 			scene.add(bowlingBall);
 			
-			// bowling pin		
-			bowlingPin = loader.parse(pinJSON);
+			// bowling pin
 			
-			bowlingPin.position.set(-3, 0, 0);
+			bowlingPin = loader.parse(pinJSON);		// use loader to parse JSON model to Object3D-object
+			bowlingPin.position.set(-3, 0, 0);		// set pin xyz position
+			// enable shadows on pin
 			bowlingPin.castShadow = true;
 			bowlingPin.receiveShadow = true;
-			
+			// add pin to scene
 			scene.add(bowlingPin);
 		}
 	}
 	
 	var render = function () {
 		requestAnimationFrame( render );
-
+		
+		// check for undefined variables to suppress errors while initializing
 		if(bowlingBall) {
 			bowlingBall.rotation.x += 0.01;
 			bowlingBall.rotation.y += 0.01;
@@ -141,6 +158,7 @@
 		}
 	};
 	
+	// window resize handler function
 	function onWindowResize(){
 		camera.aspect = window.innerWidth / window.innerHeight;
 		camera.updateProjectionMatrix();
